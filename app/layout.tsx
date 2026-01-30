e1467657a1d2a5d15b3ea059f4d38921708678ff
Luis Araujo <luis.araujo@shipeezi.com>
2026-01-30 08:58:17 -0300

    template inicial

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import '@/app/globals.css';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-      </body>
+        <GluestackUIProvider mode="dark">
+            {children}
+          </GluestackUIProvider>
+        </body>
     </html>
   );
 }
